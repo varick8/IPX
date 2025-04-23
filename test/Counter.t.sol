0c2030b8603c990009efae6fbed0e9780591f025
varick <varick.zahir@gmail.com>
2025-04-23 21:53:36 +0700

    init mas yan

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {NFT} from "../src/NFT.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
