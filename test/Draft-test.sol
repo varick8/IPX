edaf9ebbb535158c7cc9f8b12405911c06040ec6
alexchtst-01 <acinatra@gmail.com>
2025-04-25 15:09:22 +0700

    create test for register IP [all success]

---
+++ test/Draft-test.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {IPX} from "../src/Draft-IPX.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+contract IPXTest is Test, IERC721Receiver {
+    IPX public ipX;
+
+    function setUp() public {
+        ipX = new IPX();
+    }
+
+    function test_registerIP() public {
+        address owner = address(this);
+
+        uint256 tokenId1 = ipX.registerIP(
+            "Title",
+            "Description",
+            1,
+            "Tag",
+            "ipfs://hash",
+            1,
+            1,
+            10
+        );
+
+        uint256 tokenId2 = ipX.registerIP(
+            "Title again",
+            "Description again",
+            2,
+            "Tag again",
+            "ipfs://hash again",
+            2,
+            2,
+            20
+        );
+
+        assertEq(ipX.ownerOf(tokenId1), owner);
+        IPX.IP memory ip = ipX.getIP(tokenId1);
+
+        IPX.IP memory otherip = ipX.getIP(tokenId2);
+
+        // all equals
+        assertEq(ip.owner, owner);
+        assertEq(ip.title, "Title");
+        assertEq(ip.description, "Description");
+        assertEq(ip.category, 1);
+        assertEq(ip.tag, "Tag");
+        assertEq(ip.fileUpload, "ipfs://hash");
+        assertEq(ip.licenseopt, 1);
+        assertEq(ip.basePrice, 1);
+        assertEq(ip.royaltyPercentage, 10);
+
+        // all not equals
+        assertNotEq(otherip.title, "Title");
+        assertNotEq(otherip.description, "Description");
+        assertNotEq(otherip.category, 1);
+        assertNotEq(otherip.tag, "Tag");
+        assertNotEq(otherip.fileUpload, "ipfs://hash");
+        assertNotEq(otherip.licenseopt, 1);
+        assertNotEq(otherip.basePrice, 1);
+        assertNotEq(otherip.royaltyPercentage, 10);
+    }
+
+
+    function test_buyIP() public {
+        address owner = address(this);
+        uint256 tokenId = ipX.registerIP(
+            "My IP",
+            "Desc",
+            1,
+            "Tag",
+            "ipfs://file",
+            0,
+            2 ether,
+            5
+        );
+        // sebelum dijual masih punya owner
+        assertEq(ipX.ownerOf(tokenId), owner);
+
+        address buyer = vm.addr(1);
+        vm.deal(buyer, 10000 ether);
+        vm.prank(buyer);
+        ipX.buyIP{value: 2 ether}(tokenId);
+
+        // sesudah dijual udah ganti kepemilikan
+        assertEq(ipX.ownerOf(tokenId), buyer);
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
